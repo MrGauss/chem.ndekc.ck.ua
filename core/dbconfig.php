d1d9782458607938f58a575ff0cb0bba1b586592
mrgauss <mail4gauss@gmail.com>
2020-08-22 20:47:38 +0300

    autoupdate 2020-08-22-20:47:38

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -5,7 +5,7 @@ if( !defined('MRGAUSS') ){ echo basename(__FILE__); exit; }
 $db = new db
 (
   '127.0.0.1',   // HOST
-  '5434',   // HOST
+  '15472',   // HOST
   'chem.ndekc.ck.ua',   // DBNAME
   'chem.ndekc.ck.ua',   // DBUSER
   '$chem.ndekc.ck.ua%', // DBPASS
